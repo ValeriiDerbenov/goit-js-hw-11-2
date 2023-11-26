a46cf971e2ec5d6007f994e87817c37859a91446
Valerii <vd.alternative@gmail.com>
2023-11-26 20:18:06 +0200

    1236

--- src/index.js
+++ src/index.js
@@ -1,15 +1,96 @@
-import Notiflix from 'notiflix';
-const axios = require('axios');
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-const URL = 'https://pixabay.com/api';
-const KEY = '40826699-b7bef6c2b5cf50adc3ffa0ee2';
+import { fetchImages } from './js/request';
+import { makeMarkupGallery } from './js/makeMarkupGallery';
 
-// async function fetchImages(search, page) {
-//   const response = await axios.get(
-//     `${URL}/?${KEY}=${search}&page=${page}&per_page=40&image_type=photo&orientation=horizontal&safesearch=true`
-//   );
-//   return response.data;
-// }
+let page = 1;
+let lightbox;
+
+const form = document.querySelector('.search-form');
+const loadMoreBtn = document.querySelector('.load-more');
+const gallery = document.querySelector('.gallery');
+
+form.addEventListener('submit', onFormSubmit);
+loadMoreBtn.addEventListener('click', onLoadMoreBtnClick);
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  page = 1;
+
+  const inputValue = evt.currentTarget.elements.searchQuery.value.trim();
+
+  if (!inputValue) {
+    return Notify.info(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+  }
+
+  firstRenderMarkupGallery(inputValue);
+}
+
+function firstRenderMarkupGallery(searchIteam) {
+  fetchImages(searchIteam, page)
+    .then(response => {
+      if (!response.hits.length) {
+        return Notify.info(
+          'Unfortunately, there are not any matches. Try again, please'
+        );
+      }
+
+      if (response.hits.length < 40) {
+        loadMoreBtn.classList.add('display-none');
+      }
+
+      gallery.innerHTML = '';
+      Notify.info(`We have found ${response.totalHits} images.`);
+      gallery.insertAdjacentHTML('beforeend', makeMarkupGallery(response.hits));
+
+      if (response.hits.length === 40) {
+        loadMoreBtn.classList.remove('display-none');
+      }
+
+      lightbox = new SimpleLightbox('.gallery a');
+      scroll(0.2);
+    })
+    .catch(error => console.log(error));
+}
+
+function onLoadMoreBtnClick() {
+  page += 1;
+  renderMarkupGallery(form.elements.searchQuery.value.trim());
+}
+
+function renderMarkupGallery(search) {
+  fetchImages(search, page)
+    .then(response => {
+      if (response.hits.length < 40) {
+        Notify.info("You've already reached the end of search results.");
+        loadMoreBtn.classList.add('display-none');
+      }
+
+      gallery.insertAdjacentHTML('beforeend', makeMarkupGallery(response.hits));
+
+      lightbox.refresh();
+      scroll(2);
+    })
+    .catch(error => {
+      console.log(error);
+      Notify.info("You've already reached the end of search results.");
+      loadMoreBtn.classList.add('display-none');
+    });
+}
+
+function scroll(value) {
+  setTimeout(() => {
+    const { height: cardHeight } = document
+      .querySelector('.gallery')
+      .firstElementChild.getBoundingClientRect();
+
+    window.scrollBy({
+      top: cardHeight * value,
+      behavior: 'smooth',
+    });
+  }, 600);
+}
--- src/js/request.js
+++ src/js/request.js
@@ -1,8 +1,18 @@
 const axios = require('axios');
 
-const URL = "https://pixabay.com/api";
+const URL = 'https://pixabay.com/api';
+const KEY = '40826699-b7bef6c2b5cf50adc3ffa0ee2';
 
-export async function fetchImages (search, page) {
-    const response = await axios.get(`${URL}/?key=28408221-8884b414615b22bda0ca1828e&q=${search}&page=${page}&per_page=40&image_type=photo&orientation=horizontal&safesearch=true`);
-    return response.data;
+export async function fetchImages(search, page) {
+  const response = await axios.get(
+    `${URL}/?key=40826699-b7bef6c2b5cf50adc3ffa0ee2=${search}&page=${page}&per_page=40&image_type=photo&orientation=horizontal&safesearch=true`
+  );
+  return response.data;
 }
+
+// export async function fetchImages(search, page) {
+//   const response = await axios.get(
+//     `${URL}/?${KEY}=${search}&page=${page}&per_page=40&image_type=photo&orientation=horizontal&safesearch=true`
+//   );
+//   return response.data;
+// }
