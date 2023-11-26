9aa94db0a900e21962abba73b96b7f4bb3ad42ec
Valerii <vd.alternative@gmail.com>
2023-11-26 13:55:32 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 const URL = 'https://pixabay.com/api';
 const KEY = '40826699-b7bef6c2b5cf50adc3ffa0ee2';
 
-async function fetchImages(search, page) {
-  const response = await axios.get(
-    `${URL}/?${KEY}=${search}&page=${page}&per_page=40&image_type=photo&orientation=horizontal&safesearch=true`
-  );
-  return response.data;
-}
+// async function fetchImages(search, page) {
+//   const response = await axios.get(
+//     `${URL}/?${KEY}=${search}&page=${page}&per_page=40&image_type=photo&orientation=horizontal&safesearch=true`
+//   );
+//   return response.data;
+// }
